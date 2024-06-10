cc501d631951df6767edbc75869695c984a7e237
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2024-06-10 23:10:32 +0200

    ver 3.0

--- src/js/theme-switcher.js
+++ src/js/theme-switcher.js
@@ -1,5 +1,7 @@
-export function initializeThemeSwitcher() {
-  const themeToggle = document.getElementById('theme-toggle');
+export const themeToggle = document.querySelector('#theme-toggle');
+export const themeToggleMobile = document.querySelector('#theme-toggle-mobile');
+export const themeToggleMobilePlace = document.querySelector('#mobile-menus');
+export function initializeThemeSwitcher(themeToggle) {
   const savedTheme = localStorage.getItem('theme');
 
   if (savedTheme) {
@@ -12,10 +14,18 @@ export function initializeThemeSwitcher() {
   themeToggle.addEventListener('change', function () {
     if (themeToggle.checked) {
       document.body.className = 'dark-theme';
+      themeToggleMobilePlace.classList.add('dark-theme-mobile');
       localStorage.setItem('theme', 'dark-theme');
     } else {
       document.body.className = 'light-theme';
+      themeToggleMobilePlace.classList.remove('dark-theme-mobile');
       localStorage.setItem('theme', 'light-theme');
     }
   });
 }
+document.addEventListener('DOMContentLoaded', function () {
+  initializeThemeSwitcher(themeToggleMobile);
+});
+document.addEventListener('DOMContentLoaded', function () {
+  initializeThemeSwitcher(themeToggle);
+});
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // main.js
-
-import { initializeThemeSwitcher } from './js/theme-switcher';
+import './js/mobileMenu';
+import './js/theme-switcher';
+// import { initializeThemeSwitcher } from './js/theme-switcher';
 
 document.addEventListener('DOMContentLoaded', function () {
   initializeThemeSwitcher();
