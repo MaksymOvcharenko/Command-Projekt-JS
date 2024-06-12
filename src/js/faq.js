4e156c8febc11870fc17c6abe4371e21e57b0efb
13DDC <150695013+13DDC@users.noreply.github.com>
2024-06-12 23:49:15 +0300

    lite chages

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,27 +1,6 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
-
-new Accordion(accordions, {});
 const accordions = Array.from(
   document.querySelectorAll('.accordion-container')
 );
-
-// Ініціалізація акордеону для знайдених елементів
-accordions.forEach(container => {
-  const triggers = container.querySelectorAll('.ac-trigger');
-  triggers.forEach(trigger => {
-    trigger.addEventListener('click', () => {
-      const panel = trigger.closest('.ac').querySelector('.ac-panel');
-      if (panel.style.display === 'block') {
-        panel.style.display = 'none';
-      } else {
-        // Закриває всі інші панелі в цьому акордеоні
-        container
-          .querySelectorAll('.ac-panel')
-          .forEach(p => (p.style.display = 'none'));
-        panel.style.display = 'block';
-      }
-      console.log('Opened:', panel);
-    });
-  });
-});
+new Accordion(accordions, {});
