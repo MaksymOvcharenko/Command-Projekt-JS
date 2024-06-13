984a9b40ddf452e1e6a2fd0cc08607389b650c4d
13DDC <150695013+13DDC@users.noreply.github.com>
2024-06-13 19:52:58 +0300

    not so bad

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,21 +1,7 @@
 import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
-// const accordions = Array.from(
-//   document.querySelectorAll('.accordion-container')
-// );
-// new Accordion(accordions, {});
-const acTriggers = document.querySelectorAll('.ac-trigger');
-acTriggers.forEach(trigger => {
-  trigger.addEventListener('click', () => {
-    const panel = trigger.closest('.ac').querySelector('.ac-panel');
-    panel.style.display = panel.style.display === 'none' ? 'block' : 'none';
-    if (panel.style.display === 'block') {
-      // Close other panels
-      document.querySelectorAll('.ac-panel').forEach(otherPanel => {
-        if (otherPanel !== panel) {
-          otherPanel.style.display = 'none';
-        }
-      });
-    }
-  });
-});
+// import 'accordion-js/dist/accordion.min.css';
+
+const accordions = Array.from(
+  document.querySelectorAll('.accordion-container')
+);
+new Accordion(accordions, {});
