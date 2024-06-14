e4c56a02c0e5235be9fc2bbbe97e2081a6e4edd4
AnastasiiaPerevytska <anastasiia.zelenina.1995@gmail.com>
2024-06-14 15:41:31 +0300

    animation test

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,41 +1,20 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   const img = document.querySelector('.about-me-img');
-
-//   const observer = new IntersectionObserver(
-//     entries => {
-//       entries.forEach(entry => {
-//         if (entry.isIntersecting) {
-//           img.classList.add('show');
-//         } else {
-//           img.classList.remove('show');
-//         }
-//       });
-//     },
-//     { threshold: 0.1 }
-//   );
-
-//   observer.observe(img);
-// });
-
 document.addEventListener('DOMContentLoaded', () => {
-  const elementsToAnimate = [
-    ...document.querySelectorAll(
-      '.about-me-title, .info-title, .about-me-txt, .about-me-img'
-    ),
-  ];
+  const elementsToAnimate = document.querySelectorAll('.animation');
 
   const observer = new IntersectionObserver(
     entries => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
-          entry.target.classList.add('show');
+          entry.target.classList.add('animate');
         } else {
-          entry.target.classList.remove('show');
+          entry.target.classList.remove('animate');
         }
       });
     },
     { threshold: 0.1 }
   );
 
-  elementsToAnimate.forEach(element => observer.observe(element));
+  elementsToAnimate.forEach(element => {
+    observer.observe(element);
+  });
 });
