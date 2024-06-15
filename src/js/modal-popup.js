ed76667aff856a17f48882f8456cf12f9791062d
Illek <dvrheim@gmail.com>
2024-06-16 02:50:45 +0300

    Fix modal
    bg color
    z-index set to 2

--- src/js/modal-popup.js
+++ src/js/modal-popup.js
@@ -5,8 +5,21 @@ modalButton.addEventListener('click', () => {
   modalWindow.classList.remove('is-open');
 });
 
-window.addEventListener('click', function (event) {
+window.addEventListener('click', event => {
   if (event.target === modalWindow) {
-    modalWindow.classList.remove('is-open');
+    return hideModal();
   }
 });
+
+window.addEventListener('keydown', event => {
+  if (event.code === 'Escape') {
+    return hideModal;
+  }
+});
+
+function showModal() {
+  modalWindow.classList.add('is-open');
+}
+function hideModal() {
+  modalWindow.classList.remove('is-open');
+}
