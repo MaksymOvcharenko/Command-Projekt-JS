0f4335eed6647298ecaf11f635c2b4cf56710b63
AnnaEmkha <152389555+AnnaEmkha@users.noreply.github.com>
2024-06-16 17:03:54 +0300

    підключила імпорт модалки

--- src/js/works-contacts-form.js
+++ src/js/works-contacts-form.js
@@ -1,5 +1,6 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import { showModal } from './modal';
 
 const formData = { email: '', message: '' };
 const form = document.querySelector('.feedback-form');
@@ -9,21 +10,21 @@ const formSupportingText = document.querySelector('.form-supporting-text');
 
 window.addEventListener('DOMContentLoaded', fillText);
 
-emailInput.addEventListener('input', handleEmailInput); // слухач подій для введення емейлу
+emailInput.addEventListener('input', handleEmailInput);
 
-form.addEventListener('submit', sendData); // обробник події відправки форми
+form.addEventListener('submit', sendData);
 
 function handleEmailInput(event) {
-  const email = event.target.value; // отримуємо значення емейлу з події
+  const email = event.target.value;
 
   if (validateEmail(email)) {
-    checkmark.style.display = 'block'; // показуємо чекмарк
+    checkmark.style.display = 'block';
     emailInput.classList.remove('invalid');
-    formSupportingText.style.display = 'none'; // ховаємо текст підтримки
+    formSupportingText.style.display = 'none';
   } else {
-    checkmark.style.display = 'none'; // ховаємо чекмарк
+    checkmark.style.display = 'none';
     emailInput.classList.add('invalid');
-    formSupportingText.style.display = 'block'; // показуємо текст підтримки
+    formSupportingText.style.display = 'block'; 
   }
 }
 
@@ -33,12 +34,12 @@ function fillText() {
     const { email, message } = data;
     form.querySelector('[name="email"]').value = email;
     form.querySelector('[name="message"]').value = message;
-    handleEmailInput(); // перевірка валідності емейлу після заповнення форми
+    handleEmailInput(); 
   }
 }
 
 function sendData(event) {
-  event.preventDefault(); // зупиняємо стандартну відправку форми
+  event.preventDefault(); 
 
   const email = form.querySelector('[name="email"]').value;
   const message = form.querySelector('[name="message"]').value;
@@ -46,51 +47,47 @@ function sendData(event) {
   if (!email || !message) {
     iziToast.error({ title: 'Error', message: 'Please fill in all fields' });
     return;
-  } // перевіряємо чи всі дані введені
+  } 
   if (!validateEmail(email)) {
     iziToast.error({
       title: 'Error',
       message: 'Please enter a valid email address',
     });
     return;
-  }// перевіряємо чи  валідний email 
+  } 
   
   form.querySelector('[name="email"]').value = '';
-  form.querySelector('[name="message"]').value = '';  // очищаємо поля вводу
+  form.querySelector('[name="message"]').value = '';
   checkmark.style.display = 'none';
   emailInput.classList.remove('invalid');
-  formSupportingText.style.display = 'none'; // очищення стилів та текстів
- 
+  formSupportingText.style.display = 'none'; 
 
   iziToast.info({ title: 'Info', message: 'Sending message...', close: false });
 
   fetch('./url-for-post-request', {
-    // Відправляємо POST-запит на сервер, не знаю яке посилання
     method: 'POST',
-    body: JSON.stringify({ email, message }), // Відправка даних у форматі JSON
+    body: JSON.stringify({ email, message }),
     headers: {
-      'Content-Type': 'application/json', // Встановлення заголовка Content-Type
+      'Content-Type': 'application/json',
     },
   })
     .then(response => {
       if (response.ok) {
-        // Перевірка відповіді від сервера
-        showModal('success', 'Your message has been sent successfully.'); // Показ повідомлення, що все ок
-        form.reset(); // Очищення форми
+        showModal('success', 'Your message has been sent successfully.');
+        form.reset(); 
       } else {
         iziToast.error({
           title: 'Error',
           message: 'There was an error. Please try again.',
-        }); // Показ повідомлення, що є помилка
+        }); 
       }
     })
     .catch(error => {
-      // Обробка помилок відправлення запиту
-      console.error('Error:', error);
+       console.error('Error:', error);
       iziToast.error({
         title: 'Error',
         message: 'There was an error. Please try again.',
-      }); // Показ повідомлення про помилку
+      }); 
     });
 }
 
@@ -99,6 +96,3 @@ function validateEmail(email) {
   return pattern.test(email);
 }
 
-function showModal(type, message) {
-  // функція для показу модального вікна
-}
