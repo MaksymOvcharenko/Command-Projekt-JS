92862b4be4a5d00a887c6edd51867f62a2d9aeee
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2024-06-16 12:37:42 +0200

    nev 12

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@
 import './js/mobileMenu';
 import './js/theme-switcher';
 import './js/projects';
-// import { initializeThemeSwitcher } from './js/theme-switcher';
+import { initializeThemeSwitcher } from './js/theme-switcher';
 import './js/about-me';
 import './js/faq';
 import './js/modal-popup';
-document.addEventListener('DOMContentLoaded', function () {
-  initializeThemeSwitcher();
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//   initializeThemeSwitcher();
+// });
