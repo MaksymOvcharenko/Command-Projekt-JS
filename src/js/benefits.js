991ab674caae967b3900f233bf401f3ffea4ca9d
Illek <dvrheim@gmail.com>
2024-06-18 15:20:25 +0300

    Update animation
    Triggers once

--- src/js/benefits.js
+++ src/js/benefits.js
@@ -25,3 +25,27 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+//* Animation
+
+document.addEventListener('DOMContentLoaded', () => {
+  const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1,
+  };
+
+  const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+  const elements = document.querySelectorAll('.animation');
+  elements.forEach(el => observer.observe(el));
+});
